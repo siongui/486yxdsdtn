c26a3bee8c339015f57f3e058949117e52ed7a13
siongui <siongui@gmail.com>
2018-05-10 08:55:01 +0800

    get onpage codes

--- tool/igcodecrx/contentscript.js
+++ tool/igcodecrx/contentscript.js
@@ -1,1 +1,7 @@
-console.log("hello");
+var urls = [];
+var divs = document.querySelectorAll("div._mck9w._gvoze._tn0ps");
+for (var i = 0; i < divs.length; i++) {
+  var div = divs[i];
+  urls.push(div.firstChild["href"]);
+}
+console.log(urls);
